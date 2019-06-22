22604adeddb371a541235e1238e1d88512ba8b1e
fareeda123456 <faree111@gmail.com>
2019-06-22 20:34:54 +0530

    added

--- App/src/main/java/com/app/App/App.java
+++ App/src/main/java/com/app/App/App.java
@@ -1,14 +1,13 @@
 package com.app.App;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;;
+
 
 public class App 
 {
-	private static final Logger log=LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        log.info("this is simple information test log");
+       
     }
 }
